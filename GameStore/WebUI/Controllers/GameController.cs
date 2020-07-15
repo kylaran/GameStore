4c384348c814f4cc21a4409f3d590876d5cb5699
Andrey Alkhovik <kylaran70@gmail.com>
2020-07-15 15:08:15 +0300

    Online store selling games

---
+++ GameStore/WebUI/Controllers/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        private IGameRepository repository;
+        public int pageSize = 4;
+        public GameController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List(int page = 1)
+        {
+            GamesListViewModel model = new GamesListViewModel
+            {
+                Games = repository.Games
+                    .OrderBy(game => game.GameId)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Games.Count()
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ GameStore/WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using Domain.Entities;
+using Domain.Abstract;
+using Domain.Concrete;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
+        }
+    }
+}
